10a9b375b4564c8ed8c7e898d0a9233a39d44936
A.Hussin <abdalrhmanhussin44@gmail.com>
2025-09-27 10:23:58 +0300

    Install Shadcn

---
+++ src/blocks/headers/toolbar.tsx
@@ -0,0 +1,11 @@
+let toolbaritem = function() {
+    return (
+        <div className="flex gap-2">
+            <button className="px-2 py-1 bg-blue-500 text-white rounded">Button 1</button>
+            <button className="px-2 py-1 bg-blue-500 text-white rounded">Button 2</button>
+            <button className="px-2 py-1 bg-blue-500 text-white rounded">Button 3</button>
+        </div>
+    );
+}
+
+export default toolbaritem;
---
+++ src/modules/Category/types.ts
@@ -0,0 +1,9 @@
+type CategoryType = {
+    id: number;
+    name: string;
+    description: string;
+    createdAt: string;
+    updatedAt: string;
+}
+
+export type { CategoryType };
